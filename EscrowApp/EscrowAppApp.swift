6f91585911c87166d50a197c6fe14b84e9d54eb2
youny626 <zhiru@uchicago.edu>
2025-05-08 01:27:18 -0500

    Initial Commit

---
+++ EscrowApp/EscrowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscrowAppApp.swift
+//  EscrowApp
+//
+//  Created by Zhiru Zhu on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EscrowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EscrowAppTests/EscrowAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  EscrowAppTests.swift
+//  EscrowAppTests
+//
+//  Created by Zhiru Zhu on 5/8/25.
+//
+
+import Testing
+
+struct EscrowAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
