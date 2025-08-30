80cf2432e438847284e43959125f16d104c0c4cb
youny626 <zhiru@uchicago.edu>
2025-08-30 03:29:24 -0500

    update

--- Benchmark/OverheadBench.swift
+++ Benchmark/OverheadBench.swift
@@ -635,7 +635,7 @@ struct OverheadBenchRunner {
                 return (a, c)
             }
         )
-        //        ContactSeeder.reset(to: 0)
+        ContactSeeder.reset(to: 0)
         locMgr.stopUpdatingLocation()
     }
 
@@ -650,7 +650,7 @@ struct OverheadBenchRunner {
                 }
             }
             // Cleanup photos once after all sizes
-            //            PhotoSeeder.reset(to: 0)
+            PhotoSeeder.reset(to: 0)
         }
     }
 }
